44adff352e4d6014f4bc65c1739b4672dafd0256
dk3003 <38868675+dk3003@users.noreply.github.com>
2023-01-25 22:23:15 +0530

    ## Smallest subarray length contains min and max elements

---
+++ Closest min & max values in a given array.py
@@ -0,0 +1,30 @@
+import sys
+arr=[1,2,3,1,3,4,6,4,6,3]
+minimum=sys.maxsize
+maximum=0
+
+## Finding minimum and maximum value in an array
+for i in range(len(arr)):
+    if arr[i]>maximum:
+        maximum=arr[i]
+    elif arr[i]<minimum:
+        minimum=arr[i]
+
+### if all the elements in an array is same then min==max,so return 1
+if minimum==maximum:
+    print(1)
+
+### Follow the format 1. [min....max] , 2. [max...min] in a given array and find the minimum length of subrray contains min and max.
+else:
+    n=len(arr)
+    final=sys.maxsize
+    for i in range(len(arr)):
+        if arr[i]==minimum:
+            for j in range(i+1,n):
+                if arr[j]==maximum:
+                    final=min(final,j-i+1)
+        if arr[i]==maximum:
+            for j in range(i+1,n):
+                if arr[j]==minimum:
+                    final=min(final,j-i+1)
+    print(final)
