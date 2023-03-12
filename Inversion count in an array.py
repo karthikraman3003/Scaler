6916f2a9e491200e700974ecb22fbd1fc3569e7a
dk3003 <38868675+dk3003@users.noreply.github.com>
2023-03-12 19:47:00 +0530

    ## Searching - Binary search

---
+++ Inversion count in an array.py
@@ -0,0 +1,44 @@
+count=0
+def merge(arr, start, middle, end):
+    p1=start
+    p2=middle+1
+    p3=0
+    global count
+    sorted_arr=[0]*(end-start+1)
+
+    while p1<=middle and p2<=end:
+        if arr[p1]<=arr[p2]:
+            sorted_arr[p3]=arr[p1]
+            p1+=1
+        else:
+            sorted_arr[p3]=arr[p2]
+            p2+=1
+            count=count+middle-p1+1
+        p3+=1
+    while p1<=middle:
+        sorted_arr[p3]=arr[p1]
+        p1+=1
+        p3+=1
+    while p2<=end:
+        sorted_arr[p3]=arr[p2]
+        p2+=1
+        p3+=1
+    arr[start:end + 1] = sorted_arr
+
+
+def mergesort(arr,start,end):
+    if start==end:
+        return
+    middle=(start+end)//2
+
+    mergesort(arr,start,middle)
+    mergesort(arr,middle+1,end)
+    merge(arr,start,middle,end)
+
+
+if __name__ == '__main__':
+    A =[ 28, 18, 44, 49, 41, 14 ]
+    start=0
+    end=len(A)-1
+    mergesort(A,start,end)
+    print(count)
---
+++ Search for a Range.py
@@ -0,0 +1,63 @@
+# Problem Description
+# Given a sorted array of integers A (0-indexed) of size N, find the starting and the ending position of a given integer B in array A.
+#
+# Your algorithm's runtime complexity must be in the order of O(log n).
+#
+# Return an array of size 2, such that the first element = starting position of B in A and the second element = ending position of B in A, if B is not found in A return [-1, -1].
+#
+# Example
+# Input
+# Input
+# 1:
+#
+# A = [5, 7, 7, 8, 8, 10]
+# B = 8
+# Input
+# 2:
+#
+# A = [5, 17, 100, 111]
+# B = 3
+#
+# Example
+# Output
+# Output
+# 1:
+#
+# [3, 4]
+# Output
+# 2:
+#
+# [-1, -1]
+arr=[5, 17, 100, 111]
+n=len(arr)-1
+ans=-1
+low=0
+high=n
+k=3
+while low<=high:
+    middle=(low+high)//2
+
+    if arr[middle]==k:
+        ans=middle
+        high=middle-1
+    if arr[middle]<k:
+        low=middle+1
+    if arr[middle]>k:
+        high=middle-1
+low =0
+high=n
+ans1=-1
+while low<=high:
+    middle=(low+high)//2
+
+    if arr[middle]==k:
+        ans1=middle
+        low=middle+1
+    if arr[middle]<k:
+        low=middle+1
+    if arr[middle]>k:
+        high=middle-1
+l=[]
+l.append(ans)
+l.append(ans1)
+print(l)
